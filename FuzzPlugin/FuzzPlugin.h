8394ebcbead04914e147060b1d133834d5e0a995
AnnaAndres28 <105457685+AnnaAndres28@users.noreply.github.com>
2025-01-21 03:57:14 +0000

    Update FuzzPlugin.h

--- FuzzPlugin/FuzzPlugin.h
+++ FuzzPlugin/FuzzPlugin.h
@@ -1,52 +1,20 @@
-/*
-  ==============================================================================
-
-   This file is part of the JUCE framework examples.
-   Copyright (c) Raw Material Software Limited
-
-   The code included in this file is provided under the terms of the ISC license
-   http://www.isc.org/downloads/software-support-policy/isc-license. Permission
-   to use, copy, modify, and/or distribute this software for any purpose with or
-   without fee is hereby granted provided that the above copyright notice and
-   this permission notice appear in all copies.
-
-   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-   REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
-   AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
-   INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
-   LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
-   OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-   PERFORMANCE OF THIS SOFTWARE.
-
-  ==============================================================================
-*/
-
 /*******************************************************************************
- The block below describes the properties of this PIP. A PIP is a short snippet
- of code that can be read by the Projucer and used to generate a JUCE project.
 
- BEGIN_JUCE_PIP_METADATA
-
- name:             GainPlugin
+ name:             FuzzPlugin
  version:          1.0.0
  vendor:           JUCE
- website:          http://juce.com
- description:      Gain audio plugin.
+ website:          https://oshe.io
+ description:      fuzz audio plugin.
+ lastUpdated:	     Jan 20 2025 by Anna Andres
 
  dependencies:     juce_audio_basics, juce_audio_devices, juce_audio_formats,
-                   juce_audio_plugin_client, juce_audio_processors,
+                   juce_audio_plugin_client, juce_audio_processors, juce_dsp,
                    juce_audio_utils, juce_core, juce_data_structures,
                    juce_events, juce_graphics, juce_gui_basics, juce_gui_extra
- exporters:        xcode_mac, vs2022
-
- moduleFlags:      JUCE_STRICT_REFCOUNTEDPOINTER=1
+ exporters:        linux makefile
 
  type:             AudioProcessor
- mainClass:        GainProcessor
-
- useLocalCopy:     1
-
- END_JUCE_PIP_METADATA
+ mainClass:        FuzzProcessor
 
 *******************************************************************************/
 
@@ -54,26 +22,30 @@
 
 
 //==============================================================================
-class GainProcessor final : public AudioProcessor
+class FuzzProcessor final : public juce::AudioProcessor
 {
 public:
 
     //==============================================================================
-    GainProcessor()
-        : AudioProcessor (BusesProperties().withInput  ("Input",  AudioChannelSet::stereo())
-                                           .withOutput ("Output", AudioChannelSet::stereo()))
+    // Constructor that lets you define input/output channels as well as parameters and their bounds
+    FuzzProcessor()
+        : juce::AudioProcessor (BusesProperties().withInput  ("Input",  juce::AudioChannelSet::stereo())
+                                           .withOutput ("Output", juce::AudioChannelSet::stereo()))
     {
-        addParameter (gain = new AudioParameterFloat ({ "gain", 1 }, "Gain", 0.0f, 2.0f, 0.5f));
-        addParameter (clip = new AudioParameterFloat ({ "clip", 1 }, "Clip", 0.0f, 10.0f, 5.0f));
+        addParameter (gain = new juce::AudioParameterFloat ({ "gain", 1 }, "Gain", 0.0f, 3.0f, 0.5f));
+        addParameter (clip = new juce::AudioParameterFloat ({ "clip", 1 }, "Clip", 0.0f, 9.0f, 5.0f));
     }
 
     //==============================================================================
+    // This function is used before audio processing. It lets you initialize variables and set up any other resources prior to running the plugin
     void prepareToPlay (double, int) override {}
+    // This function is usually called after the plugin stops taking in audio. It can deallocate any memory used and clean out buffers
     void releaseResources() override {}
 
-    void processBlock (AudioBuffer<float>& buffer, MidiBuffer&) override
+    // This is where all the audio processing happens. One buffer of audio input is handled at a time.
+    void processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer&) override
     {
-        //buffer.applyGain (*gain);
+        
         auto gainValue = gain->get();
         auto clipValue = clip->get();
         
@@ -85,49 +57,8 @@ public:
             for (int sample = 0; sample < buffer.getNumSamples(); ++sample) 
             {
                 float processedSample = channelData[sample] * gainValue; // applying gain
-                //processedSample = std::tanh(gainValue * processedSample);
                 
-                // applying hard clipping
-                if (processedSample > clipThreshold) 
-                {
-                    processedSample = clipThreshold;
-                } 
-                else if (processedSample < -clipThreshold) 
-                {
-                    processedSample = -clipThreshold;
-                }
-                
-                // write processed sample back to buffer
-                channelData[sample] = processedSample;
-            }
-        }
-    }
-
-    void processBlock (AudioBuffer<double>& buffer, MidiBuffer&) override
-    {
-        //buffer.applyGain ((float) *gain);
-        auto gainValue = gain->get();
-        auto clipValue = clip->get();
-        
-        float clipThreshold = 0.05f / clipValue;
-        
-        for (int channel = 0; channel < buffer.getNumChannels(); ++channel) 
-        {
-            auto* channelData = buffer.getWritePointer(channel);
-            for (int sample = 0; sample < buffer.getNumSamples(); ++sample) 
-            {
-                float processedSample = channelData[sample] * gainValue; // applying gain
-                //processedSample = std::tanh(gainValue * processedSample);
-                
-                // applying hard clipping
-                if (processedSample > clipThreshold) 
-                {
-                    processedSample = clipThreshold;
-                } 
-                else if (processedSample < -clipThreshold) 
-                {
-                    processedSample = -clipThreshold;
-                }
+                processedSample = (processedSample > clipThreshold) ? clipThreshold : ((processedSample < -clipThreshold) ? -clipThreshold : processedSample);
                 
                 // write processed sample back to buffer
                 channelData[sample] = processedSample;
@@ -136,36 +67,54 @@ public:
     }
 
     //==============================================================================
-    AudioProcessorEditor* createEditor() override          { return new GenericAudioProcessorEditor (*this); }
+    // DO NOT CHANGE ANY OF THESE
+    // This creates the GUI editor for the plugin
+    juce::AudioProcessorEditor* createEditor() override          { return new juce::GenericAudioProcessorEditor (*this); }
+    // We have a GUI editor for the plugin so we return true
     bool hasEditor() const override                        { return true;   }
 
     //==============================================================================
-    const String getName() const override                  { return "Distortion PlugIn"; }
+    const juce::String getName() const override                  { return "Fuzz PlugIn"; }
+    // This function returns a boolean for whether or not the plugin accepts Midi input. We don't. so this will be false
     bool acceptsMidi() const override                      { return false; }
+    // This function returns a boolean for whether or not the plugin has Midi output. We don't. so this will be false
     bool producesMidi() const override                     { return false; }
-    double getTailLengthSeconds() const override           { return 0; }
+    // This specifies how much longer there is output when the input stops. This would be helpful for reverb/delay but not so much for distortion/gain
+    // A 0 tail length means that the output stops as soon as the input stops
+    double getTailLengthSeconds() const override           { return 0; } //TODO: Change tail length if desired
 
     //==============================================================================
+    // DO NOT CHANGE ANY OF THESE
+    // This returns the number of presets/configurations for the plugin. We only have a default configuration so we return 1
     int getNumPrograms() override                          { return 1; }
+    // This returns the index of the currently selected program. This will always be 0 for this plugin
     int getCurrentProgram() override                       { return 0; }
+    // This allows the user to switch to a different program if you have multiple
     void setCurrentProgram (int) override                  {}
-    const String getProgramName (int) override             { return "None"; }
-    void changeProgramName (int, const String&) override   {}
+    // This gives you the name of the program for a given index
+    const juce::String getProgramName (int) override             { return "None"; }
+    // This allows you to change the name of a program at the given index
+    void changeProgramName (int, const juce::String&) override   {}
 
     //==============================================================================
-    void getStateInformation (MemoryBlock& destData) override
+    // This function saves the current state of each parameter to memory so that we can load the state of each parameter 
+    // in the next session of running the pedal
+    void getStateInformation (juce::MemoryBlock& destData) override
     {
-        MemoryOutputStream (destData, true).writeFloat (*gain);
-        MemoryOutputStream (destData, true).writeFloat (*clip);
+	    juce::MemoryOutputStream (destData, true).writeFloat (*gain);
+        juce::MemoryOutputStream (destData, true).writeFloat (*clip);;
     }
 
+    // This function recalls the state of the parameters from the last session ran and restores it into the parameter
     void setStateInformation (const void* data, int sizeInBytes) override
     {
-        gain->setValueNotifyingHost (MemoryInputStream (data, static_cast<size_t> (sizeInBytes), false).readFloat());
-        clip->setValueNotifyingHost (MemoryInputStream (data, static_cast<size_t> (sizeInBytes), false).readFloat());
+        gain->setValueNotifyingHost (juce::MemoryInputStream (data, static_cast<size_t> (sizeInBytes), false).readFloat());
+        clip->setValueNotifyingHost (juce::MemoryInputStream (data, static_cast<size_t> (sizeInBytes), false).readFloat());
     }
 
     //==============================================================================
+    // This function checks to see if the requested input/output configuration is compatible with the coded plugin
+    // DO NOT CHANGE THIS FUNCTION
     bool isBusesLayoutSupported (const BusesLayout& layouts) const override
     {
         const auto& mainInLayout  = layouts.getChannelSet (true,  0);
@@ -176,9 +125,10 @@ public:
 
 private:
     //==============================================================================
-    AudioParameterFloat* gain;
-    AudioParameterFloat* clip;
+    juce::AudioParameterFloat* gain;
+    juce::AudioParameterFloat* clip;
 
     //==============================================================================
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (GainProcessor)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FuzzProcessor)
 };
+
